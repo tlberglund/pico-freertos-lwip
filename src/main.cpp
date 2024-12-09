c41903f3b5fba48825ef476026d5783f364d358f
Tim Berglund <tlberglund@gmail.com>
2024-12-09 13:48:13 -0800

    Builds, but doesn't run

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,48 @@
 #include <stdio.h>
+#include <array>
 #include "pico/stdlib.h"
 #include "GPIO.hpp"
-#include <array>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "pico/cyw43_arch.h"
 
-extern "C" {
-    #include "FreeRTOS.h"
-    #include "task.h"
+
+// static pico_cpp::GPIO_Pin ledPin(25, pico_cpp::PinType::Output);
+
+
+// Perform initialisation
+int pico_led_init(void) {
+    return cyw43_arch_init();
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 }
 
 
-static pico_cpp::GPIO_Pin ledPin(25,pico_cpp::PinType::Output);
-void vTaskCode( void * pvParameters )
+#if 0
+void vTaskCode(void *pvParameters)
 {
     /* The parameter value is expected to be 1 as 1 is passed in the
     pvParameters value in the call to xTaskCreate() below. 
     configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
     */
-    for( ;; )
+    for(;;)
     {
-            ledPin.set_high();
-            vTaskDelay(1000);
-            ledPin.set_low();
-            vTaskDelay(1000);
+        // ledPin.set_high();
+        vTaskDelay(1000);
+        // ledPin.set_low();
+        // vTaskDelay(1000);
     }
 }
+#endif
 
 int main() {
+    pico_set_led(true);
+    for(;;);
 
-
+#if 0
 BaseType_t xReturned;
 TaskHandle_t xHandle = NULL;
 /* Create the task, storing the handle. */
@@ -44,5 +59,5 @@ TaskHandle_t xHandle = NULL;
     {
         configASSERT(0);    /* We should never get here */
     }
-
+#endif
 }
